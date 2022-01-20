cda8f458c16dfe589f85836a0221a1287b7aa3c2
Yutthana00 <arf101293@gmail.com>
2022-01-20 22:53:25 +0000

    Code samles added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var today = moment().format("dddd Do MMMM YYYY ");
 var currentTime = moment();
 var currentHour = moment().format("k");
 
-$(document).ready(function(){
+$(document).ready(function() {
 });
 
 $("#currentDay").append(today)
@@ -12,7 +12,6 @@ let taskInput = $(".row input[type=text]");
 $(taskInput).each(function() {
     
     var taskInputNumber = parseInt($(this).attr("id"));
-
 
     if (taskInputNumber < currentHour) {
         $(this).addClass("past")
@@ -25,20 +24,62 @@ $(taskInput).each(function() {
 }
 });
 
-$(".savebtn").click(function(){
-    event.preventDefault();
+var checkPrevious = JSON.parse(localStorage.getItem("dayPlanner"));
+	if (checkPrevious !== null) {
+	planWorkday = checkPrevious;
 
-    let hrtask = $(this).attr("hour")
-    let storeTask = $(this).prev().val();
+localStorage.setItem("dayPlanner", JSON.stringify(planWorkday));
 
-    localStorage.setItem(hrtask, JSON.stringify(storeTask));
-});
+// var saveBtn = document.querySelector(".#05");
+// var savedTask = document.querySelector(".name");
+
+// form.addEventListener("button", function(event) {
+//         event.preventDefault();
+// });
+
+// saveBtn.addEventListener('click', function (){
+//     localStorage.setItem('savedTask', savedTask.value);
+    
+//  });
+// save data to local storage
+
+
+    
+// $("#savebtn").click(function() {
+//     event.preventDefault();
+
+//     let hrtask = $(this).attr("hour")
+//     let storeTask = $(this).prev().val();
+
+//     localStorage.setItem(hrtask, JSON.stringify(storeTask));
+// });
 
 $("#05").val(localStorage.getItem("05"))
-$(".06").val(localStorage.getItem("06"))
 
 
 
+//     var addTaskEl = $('.container');
+
+//     function handleFormSubmit(event) {
+//         event.preventDefualt();
+
+//         var savedTask = $('input[name="task-input"]').val();
+
+//         if (!savedTask) {
+//             console.log('Add your task');
+//             return;
+//         }
+
+
+//     }
+
+// shoppingFormEl.on('submit', handleFormSubmit);
+
+
+
+
+
+// clear button
 
 // let clearbtn = document.querySelector('button');
 // let inputs = document.querySelectorAll('.textarea');
