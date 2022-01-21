5256f12da7b5c52d818e4e4fcfebe1f3bdfada6e
Yutthana00 <arf101293@gmail.com>
2022-01-21 03:17:54 +0000

    save btn linked to local stroage and data callback

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,85 +5,51 @@ var currentHour = moment().format("k");
 $(document).ready(function() {
 });
 
-$("#currentDay").append(today)
 
-let taskInput = $(".row input[type=text]");
-
-$(taskInput).each(function() {
+$(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
     
-    var taskInputNumber = parseInt($(this).attr("id"));
-
-    if (taskInputNumber < currentHour) {
-        $(this).addClass("past")
+    if (currentHour == timeDiv) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
     }
-    else if (taskInputNumber == currentHour) {
-        $(this).addClass("present")
-    }
-    else if (taskInputNumber > currentHour) {
-        $(this).addClass("future")
-}
-});
+  });
 
-var checkPrevious = JSON.parse(localStorage.getItem("dayPlanner"));
-	if (checkPrevious !== null) {
-	planWorkday = checkPrevious;
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
 
-localStorage.setItem("dayPlanner", JSON.stringify(planWorkday));
-
-// var saveBtn = document.querySelector(".#05");
-// var savedTask = document.querySelector(".name");
-
-// form.addEventListener("button", function(event) {
-//         event.preventDefault();
-// });
-
-// saveBtn.addEventListener('click', function (){
-//     localStorage.setItem('savedTask', savedTask.value);
-    
-//  });
-// save data to local storage
-
-
-    
-// $("#savebtn").click(function() {
-//     event.preventDefault();
-
-//     let hrtask = $(this).attr("hour")
-//     let storeTask = $(this).prev().val();
-
-//     localStorage.setItem(hrtask, JSON.stringify(storeTask));
-// });
-
-$("#05").val(localStorage.getItem("05"))
-
-
-
-//     var addTaskEl = $('.container');
-
-//     function handleFormSubmit(event) {
-//         event.preventDefualt();
-
-//         var savedTask = $('input[name="task-input"]').val();
-
-//         if (!savedTask) {
-//             console.log('Add your task');
-//             return;
-//         }
-
-
-//     }
-
-// shoppingFormEl.on('submit', handleFormSubmit);
-
-
-
-
-
-// clear button
-
-// let clearbtn = document.querySelector('button');
-// let inputs = document.querySelectorAll('.textarea');
-
-// clearbtn.addeventlistener('click', () => {
-//     inputs.forEach(input => textarea.value = '');
-// });
+  //retrieves items from local storage and sets them in proper places
+  $("#hour-5 .time-block").val(localStorage.getItem("05"));
+  $("#hour-6 .time-block").val(localStorage.getItem("06"));
+  $("#hour-7 .time-block").val(localStorage.getItem("07"));
+  $("#hour-8 .time-block").val(localStorage.getItem("08"));
+  $("#hour-9 .time-block").val(localStorage.getItem("09"));
+  $("#hour-10 .time-block").val(localStorage.getItem("10"));
+  $("#hour-11 .time-block").val(localStorage.getItem("11"));
+  $("#hour-12 .time-block").val(localStorage.getItem("12"));
+  $("#hour-13 .time-block").val(localStorage.getItem("13"));
+  $("#hour-14 .time-block").val(localStorage.getItem("14"));
+  $("#hour-15 .time-block").val(localStorage.getItem("15"));
+  $("#hour-16 .time-block").val(localStorage.getItem("16"));
+  $("#hour-17 .time-block").val(localStorage.getItem("17"));
+  $("#hour-18 .time-block").val(localStorage.getItem("18"));
+  $("#hour-19 .time-block").val(localStorage.getItem("19"));
+  $("#hour-20 .time-block").val(localStorage.getItem("20"));
+  $("#hour-21 .time-block").val(localStorage.getItem("21"));
+  $("#hour-22 .time-block").val(localStorage.getItem("22"));
+  $("#hour-23 .time-block").val(localStorage.getItem("23"));
+  $("#hour-00 .time-block").val(localStorage.getItem("00"));
+  $("#hour-1 .time-block").val(localStorage.getItem("01"));
+  $("#hour-2 .time-block").val(localStorage.getItem("02"));
+  $("#hour-3 .time-block").val(localStorage.getItem("03"));
+  $("#hour-4 .time-block").val(localStorage.getItem("04"));
